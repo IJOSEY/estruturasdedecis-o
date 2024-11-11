14b448212722a67ff2efbf8ab0d0d200fce528fd
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-11 15:53:25 +0000

    inicio

--- decisao.c
+++ decisao.c
@@ -1,1 +1,12 @@
-fundamentos das estruturas de decisão em C
+//fundamentos das estruturas de decisão em C
+#include <stdio.h>
+int main(){
+    int numero1, numero2;
+    numero1= 10;
+    numero2= 5;
+    if (numero1 < numero2){
+        printf("Número1 é maior que número2\n");
+        }
+        printf("Fora do if\n");
+    return 0;
+}
