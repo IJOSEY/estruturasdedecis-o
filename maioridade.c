015fb6f3cbe5a680eb66128c5785d3e9bcc9fea7
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-11 16:00:29 +0000

    alteração

---
+++ maioridade.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int idade;
+    printf("Digite a sua idade:\n");
+    scanf("%d",&idade);
+    if(idade>=18){
+        pritf("voce e maior de idade\n");
+        
+    }
+}
